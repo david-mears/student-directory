d4350b269d8e5d3a56509ce57b95194bac0425e1
David Mears <kieukmail@gmail.com>
2018-12-17 20:09:54 +0000

    Added options menu for three options.

--- directory.rb
+++ directory.rb
@@ -7,7 +7,7 @@ def plural
 end
 def input_students
   puts "Please enter the names of the students, followed optionally by their cohort date in curved parentheses. (If no parentheses, default cohort is November.)"
-  puts "To finish, just hit return twice"
+  puts "To finish, return an empty input (with newline)."
   # create an empty array
   $students = []
   # get the first name
@@ -37,8 +37,8 @@ def print_header
   puts firstline.center(100)
   puts ("-" * firstline.length).center(100)
 end
-def print(students)
-  students.sort_by! { |student| student[:cohort]}
+def print_students_list
+  $students.sort_by! { |student| student[:cohort]}
   $students.each.with_index do |student, i|
     puts "#{i+1}. #{student[:name].capitalize} (#{student[:cohort].capitalize})"
   end
@@ -47,8 +47,40 @@ def print_footer(names)
   puts
   puts "In sum, we have #{names.count} great student#{plural}"
 end
+def print_menu
+  puts "*** MENU:"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+def show_students
+  print_header
+  print_students_list
+  print_footer($students)
+end
+def interactive_menu
+  $students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    print_menu
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      $students = input_students
+    when "2"
+      if $students.length > 0   
+        show_students
+      else
+        puts "No students to display."
+      end
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
 
-$students = input_students
-print_header
-print($students)
-print_footer($students)
+interactive_menu
