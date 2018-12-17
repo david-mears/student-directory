4ca5701c188cc726f15bed8cc6d0884950f8a31c
David Mears <kieukmail@gmail.com>
2018-12-17 17:33:30 +0000

    Now with inputtable cohort and default cohort

--- directory.rb
+++ directory.rb
@@ -1,30 +1,53 @@
-#let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def plural
+  if $students.length == 1
+    return "" 
+  else
+    return "s"
+  end
+end
+def input_students
+  puts "Please enter the names of the students, followed optionally by their cohort date in curved parentheses. (If no parentheses, default cohort is November.)"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  $students = []
+  # get the first name
+  input = gets.chomp
+  # while the name is not empty, repeat this code
+  while !input.empty? do
+    # add the student hash to the array
+    cohort = :november
+    if (input.include? "(") && (input.include? ")")
+      open_paren = input.index('(')
+      close_paren = input.index(')')
+      cohort = input[open_paren+1...close_paren]
+      name = input[0...open_paren]
+      $students << {name: name, cohort: cohort.to_sym}
+    else
+      $students << {name: input, cohort: :november}
+    end
+    puts "Now we have #{$students.count} student#{plural}"
+    # get another name from the user
+    input = gets.chomp
+  end
+  # return the array of students
+  $students
+end
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  firstline = "The student#{plural} of Villains Academy"
+  puts firstline.center(100)
+  puts ("-" * firstline.length).center(100)
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  $students.each.with_index do |student, i|
+    puts "#{i+1}. #{student[:name].capitalize} (#{student[:cohort].capitalize})"
   end
 end
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts
+  puts "In sum, we have #{names.count} great student#{plural}"
 end
-#nothing happens until we call the methods
+
+$students = input_students
 print_header
-print(students)
-print_footer(students)
+print($students)
+print_footer($students)
