b25af33bde49b3dacc34ec9a64546b23205f7385
David Mears <kieukmail@gmail.com>
2018-12-17 20:19:53 +0000

    With save option

--- directory.rb
+++ directory.rb
@@ -5,6 +5,17 @@ def plural
     return "s"
   end
 end
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  $students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 def input_students
   puts "Please enter the names of the students, followed optionally by their cohort date in curved parentheses. (If no parentheses, default cohort is November.)"
   puts "To finish, return an empty input (with newline)."
@@ -51,6 +62,7 @@ def print_menu
   puts "*** MENU:"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 def show_students
@@ -58,15 +70,9 @@ def show_students
   print_students_list
   print_footer($students)
 end
-def interactive_menu
-  $students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    print_menu
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
+def process
+  selection = gets.chomp
+  case selection
     when "1"
       $students = input_students
     when "2"
@@ -75,11 +81,19 @@ def interactive_menu
       else
         puts "No students to display."
       end
+    when "3"
+      save_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
     end
+end
+def interactive_menu
+  $students = []
+  loop do
+    print_menu
+    process
   end
 end
 
